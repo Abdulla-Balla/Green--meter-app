37c4da79ab2319471fae6f68222a033ffcf36544
Aduno <sanp100d@gmail.com>
2022-01-30 00:00:54 -0500

    Setting up http req

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 
 import Main from './components/main.component';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(()=>{
+    // Get request to api providing data
+    fetch('localhost:8080/api/reading/1')
+      .then(response =>response.json)
+      .then(data = test)
+  })
   return (
-    <Router>
-      <Route path="/" exact component={Main}/>
-      {/* <Route path="/api/reading" component={Api}/> */}
-    </Router>
-  );
-}
+    <div>
+    </div>
+  )}
+
 
 export default App;
