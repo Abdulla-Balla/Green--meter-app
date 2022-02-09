e1ab27b08b81601bb78faad3e6a855859ae4331e
Aduno <sanp100d@gmail.com>
2022-02-09 00:00:00 -0500

    Adding navigations for webapp

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,7 +4,7 @@ import { FiMenu } from "react-icons/fi";
 import { AiOutlineClose } from "react-icons/ai";
 import { useState } from 'react' 
 import { SidebarData } from './SidebarData.js'
-
+import { IconContext } from 'react-icons'
 
 function Sidebar(){
     const [sidebar, setSidebar] = useState(false)
@@ -12,30 +12,32 @@ function Sidebar(){
     const toggleSidebar = () => setSidebar(!sidebar)
     return (
         <>
-            <div className="sidebar">
-                <Link to="#" className='menu'>
-                    <FiMenu onClick={toggleSidebar}/>
-                </Link>
-            </div>
-            <nav className={sidebar ? 'nav-menu active': 'nav-menu'}>
-                <ul className='nav-menu-items'>
-                    <li className='navbar-toggle'>
-                        <Link to='#' className='menu-bars'>
-                            <AiOutlineClose />
-                        </Link>
-                    </li>
-                    {SidebarData.map((item, idx)=>{
-                        return (
-                            <li key={idx} className={item.className}>
-                                <Link to={item.path}>
-                                    {item.icon}
-                                    <span>{item.title}</span>
-                                </Link>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </nav>
+            <IconContext.Provider value={{color:'#fff'}}>
+                <div className="sidebar">
+                    <Link to="#" className='menu'>
+                        <FiMenu onClick={toggleSidebar}/>
+                    </Link>
+                </div>
+                <nav className={sidebar ? 'nav-menu active': 'nav-menu'}>
+                    <ul className='nav-menu-items' onClick={toggleSidebar}>
+                        <li className='navbar-toggle'>
+                            <Link to='#' className='menu-bars'>
+                                <AiOutlineClose />
+                            </Link>
+                        </li>
+                        {SidebarData.map((item, idx)=>{
+                            return (
+                                <li key={idx} className={item.className}>
+                                    <Link to={item.path}>
+                                        {item.icon}
+                                        <span className='nav-option-text'>{item.title}</span>
+                                    </Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </nav>
+            </IconContext.Provider>
         </>
     )
 }
